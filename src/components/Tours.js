58b79e7de5b0a07e8075b3ef3ba50ee188b35a72
gauri37 <kumbhar.gauri317@gmail.com>
2021-07-24 16:15:47 +0530

    done

---
+++ src/components/Tours.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Tours.css';
+import Tour from './Tour';
+
+const Tours=({tours,removeTour})=>
+{
+     return ( 
+        <section>
+            <div className="title">
+                <h1>Our Tours</h1>
+                <div className="underline"></div>
+            </div>
+            <div>
+                {tours.map((tour)=>{
+                    return <Tour key={tour.id} {...tour} removeTour={removeTour}/>
+                })}
+            </div>
+        </section>
+     );
+}
+
+export default Tours;
